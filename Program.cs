309492cd828fa70429c769250537ae44c19e7e73
StevenSuv <89829762+StevenSuv@users.noreply.github.com>
2021-09-08 12:44:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RandomNumberGenerators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int MyRandomNumber = rnd.Next(1, 11);
+
+            //programm genereerib juhuslikku numbrit 1-10
+            //programm kontrollib, kas genereeritud number on suurem kui 5
+            //kui number on suurem, kui 5, siis konsool kuvab "Random number is {MyRandomNumber}, see on suurem, kui 5
+            //kui number on 5, siis konsool kuvab "Random number is 5"
+
+            Console.WriteLine($"The computer has generated the number: {MyRandomNumber}");
+
+            if(MyRandomNumber > 5)
+            {
+                Console.WriteLine($"Random number is {MyRandomNumber}, which is bigger than 5");
+            }
+            else if (MyRandomNumber < 5)
+            {
+                Console.WriteLine($"Random number is {MyRandomNumber}, which is smaller than 5");
+            }
+            else
+            {
+                Console.WriteLine("Random number is 5");
+            }
+
+        }
+    }
+}
